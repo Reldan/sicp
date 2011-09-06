9d4287b1f1d3ee0e740869d6d2d2742c4b19d148
Reldan(Eldar Nugaev) <eldr@ya.ru>
2011-09-06 06:08:41 +0400

    first commit

---
+++ src/ch1/t1.clj
@@ -0,0 +1,35 @@
+(ns ch1.t1)
+
+(println 10)
+
+(println (+ 5 3 4))
+
+(println (- 9 1))
+
+(println (/ 6 2))
+
+(println (+ (* 2 4) (- 4 6)))
+
+(println (def a 3))
+
+(println (def b (+ a 1)))
+
+(println (+ a b (* a b)))
+
+(println (= a b))
+
+(println (if (and (> b a) (< b (* a b)))
+           b
+           a))
+
+(println (cond (= a 4) 6
+               (= b 4) (+ 6 7 a)
+               :else 25
+           ))
+
+(println (+ 2 (if (> b a) b a)))
+
+(println (* (cond (> a b) a
+                  (< a b) b
+                  :else -1)
+            (+ a 1)))
